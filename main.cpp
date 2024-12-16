2801414e823002656d7a7908377581230f36a8d9
IcheVell <ichivell@mail.ru>
2024-12-16 21:34:17 +0700

    changed vector type to size_t

--- main.cpp
+++ main.cpp
@@ -14,34 +14,34 @@ uint64_t rdtsc() {
     return (static_cast<uint64_t>(hi) << 32) | lo;
 }
 
-void fill_forward(int* array, size_t N) {
+void fill_forward(size_t* array, size_t N) {
     for (size_t i = 0; i < N - 1; i++) {
-        array[i] = static_cast<int>(i + 1);
+        array[i] = i + 1;
     }
     array[N - 1] = 0;
 }
 
-void fill_reverse(int* array, size_t N) {
+void fill_reverse(size_t* array, size_t N) {
     for (size_t i = 1; i < N; i++) {
-        array[i] = static_cast<int>(i - 1);
+        array[i] = i - 1;
     }
-    array[0] = static_cast<int>(N - 1);
+    array[0] = N - 1;
 }
 
-void fill_random(int* array, size_t N) {
+void fill_random(size_t* array, size_t N) {
     std::vector<size_t> indices(N);
     for (size_t i = 0; i < N; i++) {
         indices[i] = i;
     }
     shuffle(indices.begin(), indices.end(), std::mt19937(std::random_device{}()));
     for (size_t i = 0; i < N - 1; i++) {
-        array[indices[i]] = static_cast<int>(indices[i + 1]);
+        array[indices[i]] = indices[i + 1];
     }
-    array[indices[N - 1]] = static_cast<int>(indices[0]);
+    array[indices[N - 1]] = indices[0];
 }
 
-double measure_time(int* array, size_t N, size_t K) {
-    volatile int k = 0;
+double measure_time(size_t* array, size_t N, size_t K) {
+    volatile size_t k = 0;
     uint64_t start = rdtsc();
     for (size_t i = 0; i < N * K; i++) {
         k = array[k];
@@ -51,22 +51,21 @@ double measure_time(int* array, size_t N, size_t K) {
 }
 
 int main() {
-    size_t Nmin = 256;  
-    size_t Nmax = 4 * 1024 * 1024;
-    size_t K = 1000;
-    size_t L2 = 7 * 1024 * 1024;
-    int num_trials = 1;
+    size_t Nmin = 128;
+    size_t Nmax = 32 * 1024 * 1024 / sizeof(size_t);
+    size_t K = 100;
+    size_t L2 = 7 * 1024 * 1024 / sizeof(size_t);
+    int num_trials = 2;
 
     std::cout << "Размер массива\tПрямой обход\tОбратный обход\tСлучайный обход\n";
-    std::vector<int> array(L2);
+    std::vector<size_t> array(L2);
     fill_forward(array.data(), L2);
     fill_reverse(array.data(), L2);
     fill_random(array.data(), L2);
 
-    for (size_t N = Nmin; N <= Nmax; N *= 2) {
+    for (size_t N = Nmin; N <= Nmax; N *= 1.4) {
         array.resize(N);
-        fill_forward(array.data(), Nmin);
-        double min_times[3] = {100, 100, 100};
+        double min_times[3] = {100000, 100000, 100000};
         for (int method = 0; method < 3; method++) {
             for (int trial = 0; trial < num_trials; trial++) {
                 if (method == 0) {
@@ -83,10 +82,10 @@ int main() {
                 }
             }
         }
-        std::cout << (N * sizeof(int)) / 1024 << "\t\t"
-             << min_times[0] << "\t\t"
-             << min_times[1] << "\t\t"
-             << min_times[2] << "\n";
+        std::cout << (N * sizeof(size_t)) / 1024 << "\t\t"
+                  << min_times[0] << "\t\t"
+                  << min_times[1] << "\t\t"
+                  << min_times[2] << "\n";
     }
 
     return 0;
